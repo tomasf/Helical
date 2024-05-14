59b028c0b8cff1efa476cb9b438108d598e5e6aa
Tomas Franzén <tomas@tomasf.se>
2024-05-14 14:01:05 +0200

    Make Countersink and Counterbore properly public

--- Sources/Helical/Holes/BoltHeadRecess.swift
+++ Sources/Helical/Holes/BoltHeadRecess.swift
@@ -5,11 +5,21 @@ public protocol BoltHeadRecess: Shape3D {
 }
 
 public struct Countersink {
-    let angle: Angle
-    let topDiameter: Double
+    public let angle: Angle
+    public let topDiameter: Double
+
+    public init(angle: Angle = 90°, topDiameter: Double) {
+        self.angle = angle
+        self.topDiameter = topDiameter
+    }
 }
 
 public struct Counterbore {
-    let diameter: Double
-    let depth: Double
+    public let diameter: Double
+    public let depth: Double
+
+    public init(diameter: Double, depth: Double) {
+        self.diameter = diameter
+        self.depth = depth
+    }
 }
